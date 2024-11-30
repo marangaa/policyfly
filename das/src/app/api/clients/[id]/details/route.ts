3aad189b4edde662c397f8cc39c364c7785b1226
marangaa <rchdmaranga@gmail.com>
2024-11-30 15:11:07 +0300

    refactor: update parameter destructuring in client details route for improved clarity

--- das/src/app/api/clients/[id]/details/route.ts
+++ das/src/app/api/clients/[id]/details/route.ts
@@ -3,10 +3,10 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } },
+  context: { params: { id: string } }
 ) {
   try {
-    const clientId = params.id;
+    const clientId = context.params.id;
 
     const client = await prisma.client.findUnique({
       where: { id: clientId },
